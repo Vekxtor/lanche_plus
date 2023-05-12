3d686e8c42f60f04d2fe6a5c561d7ca850fad822
vekxtor <victoraugustu0414@gmail.com>
2023-05-12 10:53:44 -0300

    lista de produtos e printagem da mesma

---
+++ index.py
@@ -0,0 +1,5 @@
+lista_produtos = ["coxinha", "arroz", "feijão", "pao de queijo", "sardinha enlatada"]
+for exibe_lista in lista_produtos:
+    print(exibe_lista.upper())
+
+# Exibe o nome e de todos os produtos
