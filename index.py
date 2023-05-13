ffe4ca2e1468ef625bae92a80a82e1785710c4cf
vekxtor <victoraugustu0414@gmail.com>
2023-05-13 08:03:39 -0300

    Atualização no sistema de escolha, o mesmo agora funciona com função

--- index.py
+++ index.py
@@ -4,29 +4,18 @@ lista_produtos = {"coxinha": 1, "arroz": 2, "feijão": 3, "pao de queijo": 4, "s
 for produtos, valor in lista_produtos.items(): 
     print(f'{valor}) {produtos.upper()}')
 
+# FUNÇÕES
+def Escolher(num):
+    if escolha == num:
+        for produtos, valor in lista_produtos.items():
+            if escolha == valor:
+                print(f'Você escolheu \033[33m{produtos}\033[37m')
+
 # SISTEMA DE ESCOLHA DE PRODUTOS
 while True:
     escolha = int(input("Escolha um produto: "))
+    carrinho = {}
 
-    if escolha == 1:
-        for produtos, valor in lista_produtos.items():
-            if escolha == valor:
-                print("Você escolheu a Coxinha")
-    if escolha == 2:
-        for produtos, valor in lista_produtos.items():
-            if escolha == valor:
-                print("Você escolheu o Arroz")
-    if escolha == 3:
-        for produtos, valor in lista_produtos.items():
-            if escolha == valor:
-                print("Você escolheu o Feijão")
-    if escolha == 4:
-        for produtos, valor in lista_produtos.items():
-            if escolha == valor:
-                print("Você escolheu o Pão de Queijo")
-    if escolha == 5:
-        for produtos, valor in lista_produtos.items():
-            if escolha == valor:
-                print("Você escolheu a Sardinha Enlatada")
+    Escolher(escolha)
 
     break
