00ce86f08a403265cdb142ab23c6561deed08a17
vekxtor <victoraugustu0414@gmail.com>
2023-05-14 10:30:37 -0300

    Atualização do menu

--- index.py
+++ index.py
@@ -1,16 +1,22 @@
 import sqlite3
-conexao = sqlite3.connect('lanche_plus\BancoProdutos.sqlite3')
+conexao = sqlite3.connect('BancoProdutos.sqlite3')
 cursor = conexao.cursor()
-#COMANDOS SQL
+#COMANDOS SQL EM FUNÇÕES
 def Menu():
     cursor.execute("SELECT id_produto, nm_produto, vlr_produto FROM Produto;")
     produtos = []
     for produtos in cursor.fetchall():
-        print(f"{produtos[0]} - {produtos[1]} - {produtos[2]}")
+        print(f"{produtos[0]}) {produtos[1]} - R${produtos[2]}")
 
+def Escolha():
+    escolha = int(input("Digite o número correspondete à sua escolha: "))    
 
 # SISTEMA DE ESCOLHA DE PRODUTOS
-Menu()
+while True:
+    print('-'*30+'\n\033[33mESSE É O CARDÁPIO DA LANCHE+\033[37m\n'+'-'*30)
+    Menu()
 
-conexao.commit()
-conexao.close()
+
+    conexao.commit()
+    conexao.close()
+    
