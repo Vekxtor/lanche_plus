8b52263ccfeac50fe08d83787f9b61f9ce19577d
vekxtor <victoraugustu0414@gmail.com>
2023-05-15 10:53:31 -0300

    Login Criado com sucesso

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
 import sqlite3
-conexao = sqlite3.connect('BancoProdutos.sqlite3')
+conexao = sqlite3.connect('lanche_plus\BancoProdutos.sqlite3')
 cursor = conexao.cursor()
 #COMANDOS SQL EM FUNÇÕES
 def Menu():
@@ -16,12 +16,26 @@ def Escolha():
         if escolha == escolha:
             print(f'{escolha} = {produto_banco}')
 
+def Login():
+    cursor.execute("SELECT senha, nm_usuario FROM Usuario")
+    usuario = int(input("DIGITE SUA SENHA: "))
+    login = []
+    print('CARREGANDO INFORMAÇÕES...')
+    for login in cursor.fetchall():
+        if usuario == login[0]:
+            print(f'OPERADOR: {login[1]}')
+            Comanda()
+
+        else:
+            print(f'USUÁRIO NÃO LOCALIZADO NO BANCO DE DADOS')
+        break
+    
+def Comanda():
+    return True
 
 # SISTEMA DE ESCOLHA DE PRODUTOS
-while True:
-    print('-'*30+'\n\033[33mESSE É O CARDÁPIO DA LANCHE+\033[37m\n'+'-'*30)
-    Menu()
-    Escolha()
+print('-'*30+'\n\033[33mBEM VINDO AO SISTEMA DE LANÇAMENTO DA LANCHE+\033[37m\n'+'-'*30)
+Login()
 
-    conexao.commit()
-    conexao.close()
+conexao.commit()
+conexao.close()
